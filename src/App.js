ab2d4f21da4b30acaad12333192790b690efba9a
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-02-25 13:20:52 -0500

    Refactor calculator component
    Refactor calc to functional component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
