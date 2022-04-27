fa547e245c84d2f0440201d699e3b52d20e2d9d9
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-04-27 11:05:11 -0500

    Test Calcultor Component using jest testing library

---
+++ src/__test__/Calculator.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Calculator from '../components/calculator';
+import '@testing-library/jest-dom';
+
+it('renders correctly', () => {
+  const tree = renderer.create(<Calculator />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+describe('Perform calculation operations', () => {
+  test('Click one  number to display on screen', () => {
+    render(<Calculator />);
+    const result = screen.getByTestId('cal-screen-out');
+    userEvent.click(screen.getByText('7'));
+    expect(result.innerHTML).toMatch('7');
+  });
+  test('Click equals button to calculate and render result of operation on screen', () => {
+    render(<Calculator />);
+    const result = screen.getByTestId('cal-screen-out');
+    userEvent.click(screen.getByText('2'));
+    userEvent.click(screen.getByText('4'));
+    userEvent.click(screen.getByText('+'));
+    userEvent.click(screen.getByText('4'));
+    userEvent.click(screen.getByText('='));
+    expect(result.innerHTML).toMatch('28');
+  });
+  test('Click two numbers to display output on screen', () => {
+    render(<Calculator />);
+    const result = screen.getByTestId('cal-screen-out');
+    userEvent.click(screen.getByText('5'));
+    userEvent.click(screen.getByText('2'));
+    expect(result.innerHTML).toMatch('52');
+  });
+
+  test('Click modulus button to see remainder of two numbers on screen', () => {
+    render(<Calculator />);
+    const result = screen.getByTestId('cal-screen-out');
+    userEvent.click(screen.getByText('AC'));
+    userEvent.click(screen.getByText('4'));
+    userEvent.click(screen.getByText('0'));
+    userEvent.click(screen.getByText('%'));
+    userEvent.click(screen.getByText('2'));
+    expect(result.innerHTML).toMatch('0');
+  });
+});
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,7 +5,7 @@ function Calculator() {
   const [state, setState] = useState({ total: '0', next: null, operation: null });
 
   const handleClick = (e) => {
-    const buttonContents = e.target.innerText;
+    const buttonContents = e.target.textContent;
     setState((prevState) => ({
       ...prevState,
       total: buttonContents,
@@ -14,8 +14,7 @@ function Calculator() {
     setState(result);
   };
 
-  const { total } = state;
-  const { next } = state;
+  const { total, next, operation } = state;
   const onClick = (e) => { handleClick(e); };
 
   return (
@@ -25,7 +24,7 @@ function Calculator() {
         <div className="calculator">
           <div className="w-96 h-auto bg-white rounded-2xl shadow-xl">
             <div className="w-auto h-auto">
-              <div className="pr-1 btn-zinc col-span-4 text-right cal-screen">{ next || total }</div>
+              <div className="pr-1 btn-zinc col-span-4 text-right cal-screen" data-testid="cal-screen-out">{total || next ? `${total || ''} ${operation || ''} ${next || ''}` : 0}</div>
               <div className="flex justify-between">
                 <button type="button" className="btn-grey" key="first" onClick={onClick}>AC</button>
                 <button type="button" className="btn-grey" key="second" onClick={onClick}>+/-</button>
